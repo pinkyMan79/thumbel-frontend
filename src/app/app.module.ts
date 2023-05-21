8534137d988f7a3c674ec6a98d36d0928a7a9b7e
pinkyMan79 <48048748+pinkyMan79@users.noreply.github.com>
2023-05-21 14:04:26 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ForumsComponent } from './forums/forums.component';
+import { ForumPageComponent } from './forum-page/forum-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MediaComponent } from './media/media.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainPageComponent,
+    ForumsComponent,
+    ForumPageComponent,
+    ProfileComponent,
+    MediaComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
